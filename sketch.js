dedcef8dbd8e775ad2797a609db88a34b5099b57
Malavika2 <75882465+Malavika2@users.noreply.github.com>
2021-01-02 16:59:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var myEngine, myWorld;
+var ground;
+var ball;
+var box1,box2;
+function setup() {
+  createCanvas(400,400);
+
+  myEngine = Engine.create();
+  myWorld = myEngine.world;
+
+  box1 = new Box(200,300,50,50); 
+  box2 = new Box(240,100,50,100);
+
+  ground = new Ground(200,390,400,20);
+  
+}
+
+function draw() {
+  background(0);  
+  Engine.update(myEngine);
+
+  box1.display();
+  box2.display();
+
+  ground.display();
+  
+}
